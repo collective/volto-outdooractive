36d0769fff60197bc5eb6a810652c8cfe5f7ab9c
Mauro Amico <mauro.amico@gmail.com>
2022-03-21 16:05:15 +0100

    cleanup

--- src/OutdoorActive/OutdoorActiveEdit.jsx
+++ src/OutdoorActive/OutdoorActiveEdit.jsx
@@ -2,12 +2,8 @@ import React from 'react';
 import OutdoorActiveView from './OutdoorActiveView';
 import { Segment, Form } from 'semantic-ui-react';
 import { SidebarPortal, Field, Icon } from '@plone/volto/components';
-// import tableSVG from '@plone/volto/icons/table.svg';
 import { InlineForm } from '@plone/volto/components';
 import { OutdoorActiveSchema } from './schema';
-// import config from '@plone/volto/registry';
-// oa's javascripts are full of document.write() calls, so we need to use postscribe
-// e.g. Failed to execute 'write' on 'Document': It isn't possible to write into a document from an asynchronously-loaded external script unless it is explicitly opened.
 
 const OutdoorActiveEdit = (props) => {
   const { selected, onChangeBlock, block, data } = props;
